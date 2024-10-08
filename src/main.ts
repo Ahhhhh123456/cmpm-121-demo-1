703258b6176be3c483af0f3c64cd04e26f93b97b
Jason Li <Jason.Li8885551@gmail.com>
2024-10-08 12:52:33 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,21 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Jason's Game :)";
+
+//New button element
+const button = document.createElement("button");
+
+//Button text
+button.textContent = "Click 💀";
+
+// BUtton styles
+button.style.backgroundColor = "blue";
+button.style.color = "white";
+button.style.padding = "10px 20px";
+
+// Adds the button to the website
+document.body.appendChild(button);
+
 document.title = gameName;
 
 const header = document.createElement("h1");
